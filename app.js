10bf8409cee92ee2af8b5f70c650d6fc932f9af4
Naihon <lucaslud91@gmail.com>
2022-06-02 21:53:50 -0300

    hotfix2

--- app.js
+++ app.js
@@ -7,9 +7,19 @@ const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
 const express = require("express");
+const app = express();
+
+//connect db
+const connectDB = require("./db/connect");
+const authenticateUser = require("./middleware/authentication");
+
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
-const app = express();
+
+// error handler
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
 app.set("trust proxy", 1); //enable because we are going to be behaind a reverse proxy (uploading this api to Heroke)
 app.use(
   rateLimiter({
@@ -22,19 +32,13 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-//connect db
-const connectDB = require("./db/connect");
-const authenticateUser = require("./middleware/authentication");
-
 //routes
+app.get("/", (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
-app.use("/", (req, res) => {
-  res.send("jobs api");
-});
-// error handler
-const notFoundMiddleware = require("./middleware/not-found");
-const errorHandlerMiddleware = require("./middleware/error-handler");
+
 const { required } = require("joi");
 
 app.use(express.json());
