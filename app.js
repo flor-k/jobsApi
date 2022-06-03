d3a3ac1944c932256e659955e1fd3747808f1095
Naihon <lucaslud91@gmail.com>
2022-06-02 21:31:35 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,65 @@
+require("dotenv").config();
+require("express-async-errors");
+
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+const express = require("express");
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
+const app = express();
+app.set("trust proxy", 1); //enable because we are going to be behaind a reverse proxy (uploading this api to Heroke)
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, //a widnow of time in ms
+    max: 100, //limit each IP to x request per window
+  })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+//connect db
+const connectDB = require("./db/connect");
+const authenticateUser = require("./middleware/authentication");
+
+app.use("/", (req, res) => {
+  res.send("jobs api");
+});
+//routes
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
+
+// error handler
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const { required } = require("joi");
+
+app.use(express.json());
+// extra packages
+
+// routes
+app.get("/", (req, res) => {
+  res.send("jobs api");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ middleware/authentication.js
@@ -0,0 +1,23 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const { UnauthenticatedError } = require("../errors");
+
+const auth = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  console.log("auth validation", { authHeader });
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new UnauthenticatedError("Authentication invalid");
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+    //attach a user object (with the user data decripted data) to the job routes
+    req.user = { userId: payload.userId, name: payload.name };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError("Authentication invalid");
+  }
+};
+
+module.exports = auth;
