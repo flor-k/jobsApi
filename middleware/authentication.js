dba59656775715de1825951918c86e15d4d03a3e
Naihon <lucaslud91@gmail.com>
2022-06-04 16:35:09 -0300

    hotfix

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -4,7 +4,6 @@ const { UnauthenticatedError } = require("../errors");
 
 const auth = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  console.log("auth validation", { authHeader });
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     throw new UnauthenticatedError("Authentication invalid");
   }
