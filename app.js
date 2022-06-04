80b8b1a3e3c16b83aa3f76eb4c3bec23ac9bcbf7
JKAdministrator <julio.kania@gmail.com>
2022-06-04 17:00:29 -0300

    hotfix

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(xss());
 
 //routes
 app.get("/", (req, res) => {
-  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+  res.send('<h1>Jobs API v1</h1><a href="/api-docs">Documentation</a>');
 });
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
